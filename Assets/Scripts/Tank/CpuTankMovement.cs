e0827363876d192e67506aa9e3ff460c97d08ece
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2020-04-27 07:43:07 +0900

    敵の動きAI改良

--- Assets/Scripts/Tank/CpuTankMovement.cs
+++ Assets/Scripts/Tank/CpuTankMovement.cs
@@ -8,15 +8,37 @@ public class CpuTankMovement : TankMovement {
     [HideInInspector] public GameObject targetObject;
 
     private NavMeshAgent navMeshAgent;
+    private float movingTime = 4;
+    private float stoppingTime = 1;
 
     public override void Start() {
         base.Start();
         navMeshAgent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(MovingLoop());
     }
 
     private void Update() {
         if (navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid) {
             navMeshAgent.SetDestination(targetObject.transform.position);
         }
+    }
+
+    private IEnumerator MovingLoop() {
+        while (true) {
+            yield return StartCoroutine(Move());
+            yield return StartCoroutine(Stop());
+        }
+
+    }
+
+    private IEnumerator Move() {
+        navMeshAgent.isStopped = false;
+        yield return new WaitForSeconds(movingTime);
+    }
+
+    private IEnumerator Stop() {
+        navMeshAgent.isStopped = true;
+        yield return new WaitForSeconds(stoppingTime);
     }
 }
