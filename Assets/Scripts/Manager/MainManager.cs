6afee93badcac223f2b53838df654da86d96488a
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-05-04 01:13:27 +0900

    ステージ選択画面からタイトルに戻るメソッド実装

--- Assets/Scripts/Manager/MainManager.cs
+++ Assets/Scripts/Manager/MainManager.cs
@@ -7,19 +7,23 @@ using UniRx;
 using DG.Tweening;
 
 public class MainManager : MonoBehaviour {
-    public Button playButton;
+    public Button goToStageSelectButton;
     public Button optionButton;
     public Button statsButton;
     public Button infoButton;
     public GameObject stageSelectScrollView;
     public GameObject stageSelectScrollContent;
     public GameObject stageSelectScrollItemPrefab;
+    public Button playButton;
+    public Button backButtonInStageSelect;
+
+    private float transitionTime = 0.3f;
 
     void Start() {
-        playButton.onClick.AsObservable()
+        goToStageSelectButton.onClick.AsObservable()
             .Do(_ => {
                 var list = new List<Transform>();
-                list.Add(playButton.transform);
+                list.Add(goToStageSelectButton.transform);
                 list.Add(optionButton.transform);
                 list.Add(statsButton.transform);
                 list.Add(infoButton.transform);
@@ -29,6 +33,11 @@ public class MainManager : MonoBehaviour {
                 SetStageSelectScroll();
                 showList.Add(stageSelectScrollView.transform);
                 ShowLeft(showList);
+
+                var fadeInList = new List<Transform>();
+                fadeInList.Add(playButton.transform);
+                fadeInList.Add(backButtonInStageSelect.transform);
+                FadeIn(fadeInList);
             })
             .Subscribe();
 
@@ -43,17 +52,39 @@ public class MainManager : MonoBehaviour {
         infoButton.onClick.AsObservable()
             .Do(_ => { })
             .Subscribe();
+
+        backButtonInStageSelect.onClick.AsObservable()
+            .Do(_ => {
+                var showList = new List<Transform>();
+                showList.Add(goToStageSelectButton.transform);
+                showList.Add(optionButton.transform);
+                showList.Add(statsButton.transform);
+                showList.Add(infoButton.transform);
+                ShowLeft(showList);
+
+                var hideList = new List<Transform>();
+                hideList.Add(stageSelectScrollView.transform);
+                HideLeft(hideList);
+
+                var fadeOutList = new List<Transform>();
+                fadeOutList.Add(playButton.transform);
+                fadeOutList.Add(backButtonInStageSelect.transform);
+                FadeOut(fadeOutList);
+            })
+            .Subscribe();
     }
 
     private void HideLeft(List<Transform> transformList) {
         foreach (Transform transform in transformList) {
             float x = transform.localPosition.x;
+            float y = transform.localPosition.y;
+            float z = transform.localPosition.z;
             float distance = 1218f;
-            float time = 1f;
-            transform.DOLocalMoveX(x - distance, time)
+            transform.DOLocalMoveX(x - distance, transitionTime)
                 .SetEase(Ease.OutCubic)
                 .OnComplete(() => {
                     transform.gameObject.SetActive(false);
+                    transform.localPosition = new Vector3(x,y,z);
                 });
         }
     }
@@ -64,15 +95,35 @@ public class MainManager : MonoBehaviour {
             float y = transform.localPosition.y;
             float z = transform.localPosition.z;
             float distance = 1218f;
-            float time = 1f;
             transform.localPosition = new Vector3(x - distance,y,z);
             transform.gameObject.SetActive(true);
-            transform.DOLocalMoveX(x,time)
+            transform.DOLocalMoveX(x,transitionTime)
                 .SetEase(Ease.InCubic)
                 .OnComplete(() => {
 
                 });
         }
+    }
+
+    private void FadeIn(List<Transform> transformList) {
+        foreach (Transform transform in transformList) {
+            if(!transform.gameObject.GetComponent<CanvasGroup>())transform.gameObject.AddComponent<CanvasGroup>();
+            transform.gameObject.SetActive(true);
+            transform.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+            transform.gameObject.GetComponent<CanvasGroup>().DOFade(1,transitionTime).SetEase(Ease.InCubic);
+        }
+    }
+
+    private void FadeOut(List<Transform> transformList) {
+      foreach (Transform transform in transformList) {
+          if(!transform.gameObject.GetComponent<CanvasGroup>())transform.gameObject.AddComponent<CanvasGroup>();
+          transform.gameObject.SetActive(true);
+          transform.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+          transform.gameObject.GetComponent<CanvasGroup>()
+              .DOFade(0,transitionTime)
+              .SetEase(Ease.InCubic)
+              .OnComplete(() => transform.gameObject.SetActive(false));
+      }
     }
 
     private void SetStageSelectScroll(){
