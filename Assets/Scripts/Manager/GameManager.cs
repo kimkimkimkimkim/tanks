815fff49aec20d3893860fc2b8cae632d61c5eb8
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2020-05-29 21:46:30 +0900

    コメントアウトを削除

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -144,30 +144,6 @@ public class GameManager : MonoBehaviour {
         cpuTanks = stage.GetComponent<StageManager>().SetTanks(playerTankPrefab, playerTank, cpuTankPrefab);
     }
 
-    /*
-    private void SpawnAllTanks() {
-        SpawnPlayerTank();
-        SpawnCpuTanks();
-    }
-
-    private void SpawnPlayerTank() {
-        GameObject tank = Instantiate(playerTankPrefab, playerTank.spawnPoint.position, playerTank.spawnPoint.rotation) as GameObject;
-        playerTank.instance = tank;
-        playerTank.Setup();
-    }
-
-    private void SpawnCpuTanks() {
-        for (int i = 0; i < cpuTanks.Length; i++) {
-            GameObject tank = Instantiate(cpuTankPrefab, cpuTanks[i].spawnPoint.position, cpuTanks[i].spawnPoint.rotation) as GameObject;
-            cpuTanks[i].instance = tank;
-            cpuTanks[i].tankType = TankType.CPU1;
-            cpuTanks[i].playerTank = playerTank.instance;
-            cpuTanks[i].Setup();
-        }
-    }
-    */
-
-
     private void EnableTankControl() {
         playerTank.EnableControl();
 
