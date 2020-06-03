954b56dc9091ee67c3beca94e8d634eba7388c60
Kim Sangwoo <kim1219sangwoo@yahoo.co.jp>
2020-06-03 22:43:09 +0900

    壁の回り込み対策とインデント修正

--- Assets/Scripts/Tank/CpuTankMovement.cs
+++ Assets/Scripts/Tank/CpuTankMovement.cs
@@ -14,16 +14,16 @@ public class CpuTankMovement : TankMovement {
     private Vector3 targetPos;
     private float maxRadius = 4f;
 
-    public override void Start(){
-      base.Start();
-      sphere = GameObject.Find("Sphere");
-      point = GameObject.Find("Point");
+    public override void Start() {
+        base.Start();
+        sphere = GameObject.Find("Sphere");
+        point = GameObject.Find("Point");
 
-      if(sphere)sphere.SetActive(isDebug);
-      if(point)point.SetActive(isDebug);
-      targetObject.transform.GetChild(0).gameObject.SetActive(isDebug);
+        if (sphere) sphere.SetActive(isDebug);
+        if (point) point.SetActive(isDebug);
+        targetObject.transform.GetChild(0).gameObject.SetActive(isDebug);
 
-      StartCoroutine(MoveTank());
+        StartCoroutine(MoveTank());
     }
 
     private IEnumerator MoveTank() {
@@ -31,44 +31,63 @@ public class CpuTankMovement : TankMovement {
         yield return StartCoroutine(TankMoving());
     }
 
-    private IEnumerator TankMoving(){
+    private IEnumerator TankMoving() {
         Vector3 vector = targetPos - transform.position;
         vector = new Vector3(vector.x, 0, vector.z);
         vector = vector.normalized; //長さ1に正規化
 
         float prevDistance;
-        do{
-          prevDistance = (targetPos - transform.position).magnitude;
+        do {
+            prevDistance = (targetPos - transform.position).magnitude;
 
-          myRigidbody.MovePosition(myRigidbody.position + vector * speed);
-          tankChassis.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
-          tankTracksLeft.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
-          tankTracksRight.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
+            myRigidbody.MovePosition(myRigidbody.position + vector * speed);
+            tankChassis.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
+            tankTracksLeft.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
+            tankTracksRight.transform.rotation = Quaternion.LookRotation(vector); //向きを変更する
 
-          yield return null;
-        }while(prevDistance >= (targetPos - transform.position).magnitude);
+            yield return null;
+        } while (prevDistance >= (targetPos - transform.position).magnitude);
 
         yield return StartCoroutine(MoveTank());
     }
 
-    private void CalculateTargetPos(){
+    private void CalculateTargetPos() {
         var tankPos = transform.position;
         var landMarkPos = targetObject.transform.position;
-        var middlePoint = (tankPos + landMarkPos)/2;
+        var middlePoint = (tankPos + landMarkPos) / 2;
         middlePoint.y = 0.1f;
 
-        if(sphere)sphere.transform.position = middlePoint;
-        if(sphere)sphere.transform.localScale = new Vector3(maxRadius,0.1f,maxRadius);
+        if (sphere) sphere.transform.position = middlePoint;
+        if (sphere) sphere.transform.localScale = new Vector3(maxRadius, 0.1f, maxRadius);
 
-        var randomVector = new Vector3(1,0,0);
-        var ampli = UnityEngine.Random.Range(0, maxRadius);
-        var sheta = UnityEngine.Random.Range(0, 360);
-        randomVector *= ampli;
-        randomVector = Quaternion.Euler( 0, sheta, 0 ) * randomVector;
+        var existsObject = true;
+        var count = 0;
+        while (existsObject && count <= 100) {
+            count++;
+            var randomVector = new Vector3(1, 0, 0);
+            var ampli = UnityEngine.Random.Range(0, maxRadius);
+            var sheta = UnityEngine.Random.Range(0, 360);
+            randomVector *= ampli;
+            randomVector = Quaternion.Euler(0, sheta, 0) * randomVector;
+            targetPos = middlePoint + randomVector;
+            targetPos.y = 0.2f;
+            if (point) point.transform.position = targetPos;
 
-        targetPos = middlePoint + randomVector;
-        targetPos.y = 0.2f;
-        if(point)point.transform.position = targetPos;
-        targetPos.y = 0;
+            float y = 1;
+            targetPos.y = y;
+            landMarkPos.y = y;
+            Ray ray = new Ray(landMarkPos, targetPos - landMarkPos);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * (landMarkPos - targetPos).magnitude, Color.green, 1.0f);
+            if (Physics.Raycast(ray, out hit, (landMarkPos - targetPos).magnitude)) {
+                Debug.Log(hit.collider.name);
+                existsObject = hit.collider.name == "Wall";
+            } else {
+                //何にもぶつからない
+                existsObject = false;
+            }
+
+            targetPos.y = 0;
+        }
     }
 }
